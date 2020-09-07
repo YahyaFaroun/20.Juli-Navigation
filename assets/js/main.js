b0b6a6d4d4bc9d64a640d5ea202dd002e86469cf
Yahya Faroun <yahyafaroun@gmail.com>
2020-09-07 15:01:42 +0200

    reload

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+function ham(){
+    console.log("lauft")
+    document.getElementById("hamburger").classList.toggle("show");
+}
+
+function button1(){
+    console.log("lauft")
+    document.getElementById("textButton1").classList.toggle("textShow");
+}
+
+function button2(){
+    console.log("lauft")
+    document.getElementById("textButton2").classList.toggle("textShow");
+}
+
+
+
+
+function white(){
+    console.log("läuft");
+    document.getElementById("main").className=("white");
+}
+
+function black(){
+    console.log("läuft");
+    document.getElementById("main").className=("black");
+}
+function rose(){
+    console.log("läuft");
+    document.getElementById("main").className=("rose");
+}
---
+++ lösung/lösung.js
@@ -0,0 +1,19 @@
+function showNav() {
+    console.log("Test")
+    document.getElementById('navigation').classList.toggle('show-nav')
+}
+function showAnswer1() {
+    document.getElementById('answer1').classList.toggle('show-answer')
+}
+function showAnswer2() {
+    document.getElementById('answer2').classList.toggle('show-answer')
+}
+function changeColor1() {
+    document.getElementById('root').className = 'color1'
+}
+function changeColor2() {
+    document.getElementById('root').className = 'color2'
+}
+function changeColor3() {
+    document.getElementById('root').className = 'color3'
+}
